2e0b738d45bdc4f948569472846e436d4f2ba8b6
Vincent Gravie <vgelumenta@gmail.com>
2024-01-20 17:59:58 +0800

    portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    // container: {
+    //   center: true,
+    //   padding: '8px',
+    //   width: '100%',
+    // },
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        secondary: '#94a3b8',
+        dark: '#0f172a'
+      }
+    },
+    // screens: {
+    //   '2xl': '1320px',
+    // },
+  },
+  plugins: [],
+}
+
